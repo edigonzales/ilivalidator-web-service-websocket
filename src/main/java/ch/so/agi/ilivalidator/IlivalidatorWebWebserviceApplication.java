79e83693c42c10dbef97709d9ea543cf53c57dbd
Stefan Ziegler <edi.gonzales@gmail.com>
2019-12-23 15:58:53 +0100

    initial commit

---
+++ src/main/java/ch/so/agi/ilivalidator/IlivalidatorWebWebserviceApplication.java
@@ -0,0 +1,13 @@
+package ch.so.agi.ilivalidator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IlivalidatorWebWebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IlivalidatorWebWebserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/so/agi/ilivalidator/IlivalidatorWebWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.so.agi.ilivalidator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IlivalidatorWebWebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
