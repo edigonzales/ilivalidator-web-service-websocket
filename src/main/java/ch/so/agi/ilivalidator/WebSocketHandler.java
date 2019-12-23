6bdd07e736be9f8631bae533ee29322b3d810f68
Stefan Ziegler <edi.gonzales@gmail.com>
2019-12-23 16:38:52 +0100

    initial app commit

---
+++ src/main/java/ch/so/agi/ilivalidator/WebSocketHandler.java
@@ -0,0 +1,24 @@
+package ch.so.agi.ilivalidator;
+
+import org.springframework.web.socket.BinaryMessage;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.AbstractWebSocketHandler;
+
+import java.io.IOException;
+
+public class WebSocketHandler extends AbstractWebSocketHandler {
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws IOException {
+        System.out.println("New Text Message Received");
+        System.out.println(session.getId());        
+        session.sendMessage(message);
+    }
+
+    @Override
+    protected void handleBinaryMessage(WebSocketSession session, BinaryMessage message) throws IOException {
+        System.out.println("New Binary Message Received");
+        System.out.println(session.getId());
+        session.sendMessage(message);
+    }
+}
