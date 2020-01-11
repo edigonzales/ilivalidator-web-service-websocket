e4db1f709c7a8f6a2018eb626698d07c42be750c
Stefan Ziegler <edi.gonzales@gmail.com>
2020-01-11 19:27:38 +0100

    int tests skeleton

--- src/test/java/ch/so/agi/ilivalidator/IntegrationTests.java
+++ src/test/java/ch/so/agi/ilivalidator/IntegrationTests.java
@@ -1,21 +1,36 @@
 package ch.so.agi.ilivalidator;
 
-import java.net.URI;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import javax.websocket.ContainerProvider;
-import javax.websocket.Session;
-import javax.websocket.WebSocketContainer;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.nio.file.Files;
+import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.boot.SpringApplication;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.web.socket.client.standard.StandardWebSocketClient;
+import org.springframework.web.socket.BinaryMessage;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.WebSocketMessage;
+import org.springframework.web.socket.WebSocketSession;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -25,20 +40,110 @@ public class IntegrationTests {
     @LocalServerPort
     private String port;
     
+    @Value("#{servletContext.contextPath}")
+    private String servletContextPath;
+
     @BeforeAll
-    public static void setup() {
-        //SpringApplication.run(IlivalidatorWebServiceApplication.class, new String[0]);
+    public static void setup() {}
+
+    public class ClientSocketHandler implements WebSocketHandler {
+        Logger logger = LoggerFactory.getLogger(ClientSocketHandler.class);
+
+        private WebSocketSession webSocketSession;
+        private String returnedMessage;
+
+        @Override
+        public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+            webSocketSession = session;
+        }
+
+        @Override
+        public void handleMessage(WebSocketSession session, WebSocketMessage<?> message) throws Exception {
+            String result = message.getPayload().toString();
+            this.returnedMessage = result;
+        }
+
+        @Override
+        public void handleTransportError(WebSocketSession session, Throwable exception) throws Exception {
+            logger.error("Got a handleTransportError: " + exception.getMessage());
+            exception.printStackTrace();
+        }
+
+        @Override
+        public void afterConnectionClosed(WebSocketSession session, CloseStatus closeStatus) throws Exception {
+        }
+
+        @Override
+        public boolean supportsPartialMessages() {
+            return false;
+        }
+
+        public boolean isConnected() {
+            return webSocketSession != null;
+        }
+
+        public void sendMessage(Object msg) throws Exception {
+            if (msg instanceof File) {
+                byte[] fileContent = Files.readAllBytes(((File) msg).toPath());
+                webSocketSession.sendMessage(new BinaryMessage(fileContent));
+            } else {
+                webSocketSession.sendMessage(new TextMessage(msg.toString()));
+            }
+        }
+
+        public void closeConnection() throws IOException {
+            if (isConnected()) {
+                webSocketSession.close();
+            }
+        }
+        
+        public String getMessage() {
+            return this.returnedMessage;
+        }
     }
 
     @Test
     public void testGetLog() throws Exception {
-        String defaultEndpoint =  "ws://localhost:"+port+"/ilivalidator";
+        String endpoint = "ws://localhost:" + port + "/ilivalidator/socket";
+        logger.info(endpoint);
 
-        logger.info(defaultEndpoint);
-        logger.info("port: " + port);
-        System.out.println(defaultEndpoint);
-        Thread.sleep(100000);
+        StandardWebSocketClient client = new StandardWebSocketClient();
+        ClientSocketHandler clientHandler = new ClientSocketHandler();
+        client.doHandshake(clientHandler, endpoint);
+
+        Thread.sleep(2000);
+        assertTrue(clientHandler.isConnected());
+        
+        File file = new File("src/test/data/ch_254900.itf");
+        clientHandler.sendMessage(file);
+        clientHandler.sendMessage(file.getName());
+
+        Thread.sleep(10000);
+        
+        String returnedMessage = clientHandler.getMessage();
+        assertTrue(returnedMessage.contains("...validation done:"));
+        System.out.println(returnedMessage);
+        
+        Document document = Jsoup.parse(returnedMessage);
+        Elements links = document.select("a[href]");
+        
+        String link = links.get(0).attr("href");
+        logger.info(link);
+        
+        URL logfileUrl = new URL("http://localhost:"+port+servletContextPath+"/"+link);
+        logger.info("url: "+ logfileUrl);
+        
+        logger.info(servletContextPath);
+        
+        String logfileContents = null;
+        try (InputStream in = logfileUrl.openStream()) {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+            logfileContents = reader.lines().collect(Collectors.joining(System.lineSeparator()));
+        }
+//        logger.info(logfileContents);
+        
+
+        
     }
-
 
 }
