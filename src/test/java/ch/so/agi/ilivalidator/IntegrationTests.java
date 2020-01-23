aca1b94654da08b70b0e3f7d90757109a20fe68a
Stefan Ziegler <edi.gonzales@gmail.com>
2020-01-23 10:27:36 +0100

    add tests

--- src/test/java/ch/so/agi/ilivalidator/IntegrationTests.java
+++ src/test/java/ch/so/agi/ilivalidator/IntegrationTests.java
@@ -1,5 +1,6 @@
 package ch.so.agi.ilivalidator;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.BufferedReader;
@@ -151,13 +152,73 @@ public class IntegrationTests {
         clientHandler.sendMessage(file);
         clientHandler.sendMessage(file.getName());
 
-        Thread.sleep(60000);
+        Thread.sleep(90000);
         
         String returnedMessage = clientHandler.getMessage();
         assertTrue(returnedMessage.contains("...validation failed:"));
 
-
+        Document document = Jsoup.parse(returnedMessage);
+        Elements links = document.select("a[href]");
+        
+        String link = links.get(0).attr("href");
+        URL logfileUrl = new URL("http://localhost:"+port+servletContextPath+"/"+link);
+                
+        String logfileContents = null;
+        try (InputStream in = logfileUrl.openStream()) {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+            logfileContents = reader.lines().collect(Collectors.joining(System.lineSeparator()));
+        }
+        
+        assertTrue(logfileContents.contains("so_nutzungsplanung_20171118.toml"));
+        assertTrue(logfileContents.contains("Warning: line 5: SO_Nutzungsplanung_20171118.Rechtsvorschriften.Dokument: tid d3c20374-f6c5-48f9-8e1e-232b87a9d80a: invalid format of INTERLIS.URI value <34-Messen/Entscheide/34-36_45-E.pdf> in attribute TextImWeb"));
+        assertTrue(logfileContents.contains("Error: line 61: SO_Nutzungsplanung_20171118.Rechtsvorschriften.HinweisWeitereDokumente: tid 6: Association SO_Nutzungsplanung_20171118.Rechtsvorschriften.HinweisWeitereDokumente must not have an OID (6)"));
+        assertTrue(logfileContents.contains("Error: line 412: SO_Nutzungsplanung_20171118.Nutzungsplanung.Grundnutzung: tid 2d285daf-a5ab-4106-a453-58eef2e921ab: duplicate coord at (2599932.281, 1216063.38, NaN)"));
+        assertTrue(logfileContents.contains("Error: line 140: SO_Nutzungsplanung_20171118.Nutzungsplanung.Typ_Ueberlagernd_Flaeche: tid 0723a0c8-46e4-4e4f-aba4-c75e90bece14: Attributwert Verbindlichkeit ist nicht identisch zum Objektkatalog: 'orientierend' - '6110'"));
+        assertTrue(logfileContents.contains("Error: line 11: SO_Nutzungsplanung_20171118.Rechtsvorschriften.Dokument: tid 56bf54a5-10bd-4c6f-918f-0da92493ea7e: Dokument 'https://geo.so.ch/docs/ch.so.arp.zonenplaene/Zonenplaene_pdf/24-Brunnenthal/Reglemente/024_BZR.pdf' wurde nicht gefunden."));
+        assertTrue(logfileContents.contains("Error: line 1177: SO_Nutzungsplanung_20171118.Nutzungsplanung.Ueberlagernd_Flaeche: tid 0f55e390-9aba-4833-aaa5-c6b98fae7633: Set Constraint "));
+        assertTrue(logfileContents.contains("Info: ...validation failed"));
+        assertFalse(logfileContents.contains("Info: assume"));
     }
     
+    @Test
+    public void validation_Ok_ili2() throws Exception {
+        String endpoint = "ws://localhost:" + port + "/ilivalidator/socket";
+
+        StandardWebSocketClient client = new StandardWebSocketClient();
+        ClientSocketHandler clientHandler = new ClientSocketHandler();
+        client.doHandshake(clientHandler, endpoint);
+
+        Thread.sleep(2000);
+        assertTrue(clientHandler.isConnected());
+
+        File file = new File("src/test/data/2457_Messen_nachher.xtf");
+        clientHandler.sendMessage(file);
+        clientHandler.sendMessage(file.getName());
+
+        Thread.sleep(90000);
+        
+        String returnedMessage = clientHandler.getMessage();
+        assertTrue(returnedMessage.contains("...validation done:"));
+
+        Document document = Jsoup.parse(returnedMessage);
+        Elements links = document.select("a[href]");
+        
+        String link = links.get(0).attr("href");
+        URL logfileUrl = new URL("http://localhost:"+port+servletContextPath+"/"+link);
+                
+        String logfileContents = null;
+        try (InputStream in = logfileUrl.openStream()) {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+            logfileContents = reader.lines().collect(Collectors.joining(System.lineSeparator()));
+        }
+        
+        assertTrue(logfileContents.contains("so_nutzungsplanung_20171118.toml"));
+        assertTrue(logfileContents.contains("Warning: line 5: SO_Nutzungsplanung_20171118.Rechtsvorschriften.Dokument: tid d3c20374-f6c5-48f9-8e1e-232b87a9d80a: invalid format of INTERLIS.URI value <34-Messen/Entscheide/34-36_45-E.pdf> in attribute TextImWeb"));
+        assertTrue(logfileContents.contains("Info: additional model SO_FunctionsExt"));
+        assertTrue(logfileContents.contains("Info: ...validation done"));
+        assertFalse(logfileContents.contains("Info: assume"));
+        assertFalse(logfileContents.contains("Error"));
+    }
+
 
 }
